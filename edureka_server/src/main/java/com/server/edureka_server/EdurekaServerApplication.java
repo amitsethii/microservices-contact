2253225bd64d8983f47268ae70d758278b7c091c
amit sethi <amitsethi150@gmail.com>
2022-12-06 14:03:09 +0530

    edureka server progress

---
+++ edureka_server/src/main/java/com/server/edureka_server/EdurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.edureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdurekaServerApplication.class, args);
+	}
+
+}
